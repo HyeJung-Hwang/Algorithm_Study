f19e6c413c495f63b6ba40c1e42db74aa290f474
Hyejung Hwang <hyejung.hwang@Hyejungui-MacBookAir.local>
2026-03-23 22:31:08 +0900

    feat: 탐색은 모든 노드를 한번에 타고타고 경로를 따라가야할 떄만 필요한 것 ..

--- Concept/Prev/vol.1/07_Graph_Traversal/boj_2573.py
+++ Concept/Prev/vol.1/07_Graph_Traversal/boj_2573.py
@@ -7,27 +7,59 @@
 0 0 0 0 0 0 0
 """
 # 입력: 그래프
-# 알고리즘: BFS()
+# 알고리즘: BFS() [Mistake]  "일년마다 그 칸에 동서남북 네 방향으로 붙어있는 0이 저장된 칸의 개수만큼 줄어든다"  -> 탐색 아님 일괄 계산
 # 1) 1년 진행 후 , 멀티소스 BFS로 빙산 전부 탐색하기 ,(다른 빙산이 바다가 되는 거로 인해 녹는게 달라져서 ) 
 # 2) 덩어리 개수 세기 (dfs로 방문해서 방문경로에 현재 빙산 노드들 다 안들어가면 덩아리가 2개 이상인거로 간주 )
-
+# []
 import sys
 input = sys.stdin.readline
+from collections import queue
 
-def bfs():
-    pass
-def dfs():
-    pass
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+def bfs(queue):
+    while queue:
+        u,v = queue.popleft()
+
+        for i in range(4):
+            nx,ny = u+ dx[i], v+dy[i]
+            if 0<= nx < N and 0<= ny < M:
+                if not visited[nx][ny]: # [Mistake]계속하는 실수
+                    if graph[nx][ny] == 0:
+                        graph[u][v] -= 1
+                        visited[nx][ny] = 1
+                        queue.append((nx,ny))
+def dfs(u,v, stack = []):
+    stack.append((u,v))
+    for i in range(4):
+        nx,ny = u+ dx[i], v+dy[i]
+        if 0<= nx < N and 0<= ny < M:
+            if visited[nx][ny]:
+                dfs(nx, ny,stack)
+
+    return stack
 
 if __name__ == "__main__":
     N, M = map(int,input().strip().split(" "))
     graph = [ list(map(int,input().strip().split(" "))) for _ in range(N)]
-    dist = graph.copy()
     # print(N,M)
     # print(graph)
     year_cnt = 1
     while True:
-        # dist에 대해 bfs
-
-        # dfs로 덩어리 수 세가 (아무 시작점에 대해 )
         year_cnt += 1
+        # dist에 대해 bfs] [Mistake]
+        for i in range(N):
+            for j in range(M):
+                if graph[i][j]:
+                    queue.append((i,j))
+        visited = [ [0]*M for _ in range(N)] 
+        bfs(queue)
+        ice_list = []
+        for i in range(N):
+            for j in range(M):
+                ice_list.append((i,j))
+        now_ice_list = dfs() # 아무 빙산에 대해 시작
+        if len(now_ice_list) != len(ice_list):
+            break
+        # dfs로 덩어리 수 세가 (아무 시작점에 대해 
+    print(year_cnt)
